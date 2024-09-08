5f73e4771d507e4fd3a39b5d16b4556f729cdf0b
Mustafa Şenlik <105501454+MustafaSenliik@users.noreply.github.com>
2024-09-08 19:33:12 +0300

    Update main.rs

--- İlk Alıştırmalar/main.rs
+++ İlk Alıştırmalar/main.rs
@@ -5,4 +5,25 @@ struct Deste{
  impl Deste {
      fn new() -> Self{
         let mut kartlar = vec![]; 
-    let kart_turleri = ["Kupa", 
+    let kart_turleri = ["Kupa", "Karo","Sinek"];
+    let degerler = ["2","3","4"];
+
+    for kart_turleri in kart_turleri{
+        for degerler in degerler{
+            let kart = format!("{} {}", kart_turleri, degerler);
+            kartlar.push(kart);
+        }
+    }
+        
+
+    let deste:Deste = Deste{kartlar};
+    return deste;
+     }
+     
+ }
+fn main(){
+    
+    let deste = Deste::new();
+    
+    println!("Deste Burada: {:#?}",deste);
+}
