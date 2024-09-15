6ae283c21e8e8af1c23efd69f21affdd51daef85
Mustafa Şenlik <105501454+MustafaSenliik@users.noreply.github.com>
2024-09-15 12:44:01 +0300

    Update Deste.rs

--- İlk Alıştırmalar/main.rs
+++ İlk Alıştırmalar/main.rs
@@ -1,29 +1,36 @@
+use rand::{thread_rng, seq::SliceRandom}; // Virgül eksikti
+
 #[derive(Debug)]
-struct Deste{
-    kartlar: Vec<String>
+struct Deste {
+    kartlar: Vec<String>,
 }
- impl Deste {
-     fn new() -> Self{
-        let mut kartlar = vec![]; 
-    let kart_turleri = ["Kupa", "Karo","Sinek"];
-    let degerler = ["2","3","4"];
 
-    for kart_turleri in kart_turleri{
-        for degerler in degerler{
-            let kart = format!("{} {}", kart_turleri, degerler);
-            kartlar.push(kart);
+impl Deste {
+    fn new() -> Self {
+        let mut kartlar = vec![];
+        let kart_turleri = ["Kupa", "Karo", "Sinek"];
+        let degerler = ["2", "3", "4"];
+
+        for kart_turleri in kart_turleri {
+            for degerler in degerler {
+                let kart = format!("{} {}", kart_turleri, degerler);
+                kartlar.push(kart);
+            }
         }
-    }
-        
 
-    let deste:Deste = Deste{kartlar};
-    return deste;
-     }
-     
- }
-fn main(){
-    
-    let deste = Deste::new();
-    
-    println!("Deste Burada: {:#?}",deste);
+        Deste { kartlar }
+    }
+
+    // Shuffle fonksiyonu burada tanımlanmalı
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.kartlar.shuffle(&mut rng); // SliceRandom kullanarak shuffle yapıyoruz
+    }
+}
+
+fn main() {
+    let mut deste = Deste::new(); // Deste mutably oluşturulmalı ki shuffle edilebilsin
+    deste.shuffle(); // Shuffle işlemi
+
+    println!("Deste Burada: {:#?}", deste); // Deste yazdırılıyor
 }
