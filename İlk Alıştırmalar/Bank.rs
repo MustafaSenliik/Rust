2f687e870440752ad8bfbc5ebd181f27756b43c1
Mustafa Şenlik <105501454+MustafaSenliik@users.noreply.github.com>
2024-09-23 16:15:17 +0300

    Update Bank.rs

--- İlk Alıştırmalar/Bank.rs
+++ İlk Alıştırmalar/Bank.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
 struct Account {
     id: u32,
-    balance: i32,
     holder: String,
+    balance: i32,
 }
 
 impl Account {
@@ -13,6 +13,17 @@ impl Account {
             balance: 0,
         }
     }
+    fn summary(&self) ->String{
+        format!("{} Kullanıcı Bakiyesi Özeti {} TL",self.holder,self.balance)
+    }
+    fn deposit(&mut self, amount:i32) ->i32{
+        self.balance += amount;
+        self.balance
+    }
+    fn withdraw(&mut self,amount:i32) ->i32{
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 #[derive(Debug)]
@@ -22,19 +33,46 @@ struct Bank {
 
 impl Bank {
     fn new() -> Self {
-        Bank { accounts: vec![] }
+        Bank {
+            accounts: vec![],
+        }
     }
-}
-fn add_account(bank: &mut Bank, account:Account){
-    bank.accounts.push(account);
+    fn total_balance(&self) -> i32 {
+    self.accounts.iter().map(|account| account.balance).sum()
 }
 
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+}
+}
+
+fn add_account_print(account: Account, bank: &mut Bank) {
+    bank.accounts.push(account);
+    println!("{:#?}", bank);
+}
+
+fn account_printer(account: &Account) {
+    println!("{:#?}", account);
+}
 
 fn main() {
+    
+    let mut account = Account::new(1, String::from("Mustafa Şenlik"));
     let mut bank = Bank::new();
-    let account = Account::new(1, String::from("me"));
     
-    add_account(&mut bank, account);
+    account.deposit(500);
+    account.withdraw(250);
+    println!("{:#?}", account.summary());
+
+    // Hesabı bankaya ekliyoruz
+    add_account_print(account, &mut bank);
     
-    println!("{:#?}", bank);
+    // Bankanın özetini ve toplam bakiyesini yazdırıyoruz
+    println!("{:#?}", bank.summary());
+    println!("Toplam Bakiye: {} TL", bank.total_balance());
+
+
 }
